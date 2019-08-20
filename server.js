ccba48b26ce0943dbcf294c629794fddbc36c1f2
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-20 17:12:00 -0400

    added article js in modules

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var axios = require("axios");
 var cheerio = require("cheerio");
+
 var PORT = process.env.PORT || 8000
 var app = express();
+var databaseUrl = "NasaScraper";
+var collections = ["scrapedNews"];
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +16,6 @@ app.use(express.json());
 var exphbs = require ("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// var routes = require("./controllers/controller.js");
-// app.use(routes);
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
 });
