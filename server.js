34e9b35f6a82dbcd8ebcca2c47ce73372f158337
Aida Yrysbekova <aidiko20@gmail.com>
2019-09-20 13:39:12 -0400

    changes api routes to controllers

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 
-// var logger = require("morgan");
-var routes = require('./routes/apiRoutes');
-var routes = require('./routes/htmlRoutes');
+var express= require("express");
+var mongoose = require("mongoose");
 
 var PORT = process.env.PORT || 8000
 var app = express();
@@ -12,14 +11,14 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(routes);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 var exphbs = require ("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
-
+var routes = require("./controllers/scraper_controllers.js");
+app.use(routes);
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
 });
