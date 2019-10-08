340fed7c0c6f55a4b5c7b1c1ed56a9c66bc85d1c
Aida Yrysbekova <aidiko20@gmail.com>
2019-10-08 10:20:27 -0400

    fixed my server.js file

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-var express= require("express");
-var mongoose = require("mongoose");
 
-var PORT = process.env.PORT || 8000
+var PORT = process.env.PORT || 3000
 var app = express();
+var router = express.Router();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+app.use(express.static(_dirname + "/public"));
+
 var exphbs = require ("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-var routes = require("./controllers/scraper_controllers.js");
-app.use(routes);
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost:mongoHadlines";
+
+mongoose.connect(db, function (error) {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        console.log("mongoose connection is succssful");
+    }
+});
+
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
 });
