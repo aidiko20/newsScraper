69b2c719505f14db3d65e61895976aea39cc7445
Aida Yrysbekova <aidiko20@gmail.com>
2019-10-08 12:49:21 -0400

    fixed scrape controllers

--- controllers/scraper_controllers.js
+++ controllers/scraper_controllers.js
@@ -1,119 +1,36 @@
-var express = require("express");
-var router = express.Router();
-var axios = require("axios");
-var cheerio = require("cheerio");
-var db = require("../models");
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
 
-router.get("/", function (req, res) {
-    db.Article.find({ "saved" : false}).sort({_id: -1}).limit(10)
-    .then(function (dbArticle) {
-        var hbsObject = {
-            articles: dbArticle
-        };
-        res.render("index", hbsObject);
-    })
-    .catch(function (err) {
-        res.json(err);
-    });
-})
+var Headline = require("../models/Article");
 
-router.get("/saved", function (req, res) {
-    db.Article.find({ "saved": true}).sort({_id: -1})
-    .populate("comments")
-    .then(function (dbArticle) {
-        var hbsObject ={
-            articles: dbArticle
-        };
-        res.render("saved", hbsObject);
-    })
-    .catch(function (err) {
-        res.json(err);
-    });
-})
-
-router.get("/", function (req, res) {
-    axios.get("https://www.jpl.nasa.gov/news/")
-    .then(function (response) {
-        var $ = cheerio.load(response.data);
-        $(".image_and_description_container").each(function(i, element) {
-            var result = {};
-            result.title = $(this)
-            .children(".list_text_content")
-            .children(".content_title")
-            .text().trim();
-            result.image = $(this)
-            .children(".image_and_description_container")
-            .children("a")
-            .attr("href");
-            result.summary = $(this)
-            .children(".list_text_content")
-            .children(".article_teaser_body")
-            .text();
-            result.link = $(this)
-            .children("a")
-            .attr("href");
-
-            
-            db.Article.create(req, res)
-            .then(function (dbArticle) {
-                console.log(dbArticle);
-            })
-            .catch(function (err) {
-                // renderHeadlines()
-                console.log(err);
+module.exports = {
+    fetch: function (cb) {
+        scrape(function (data) {
+            var articles = data;
+            for (var i=0; i< articles.length; i++) {
+                articles[i].date = makeDate();
+                articles[i].saved = false;
+            }
+            Headline.collection.insertMany(articles, {ordered: false}, function (err, docs) {
+                cb( err, docs)
             });
         });
-        res.send("Scrape Complete");
-    });
-});
-router.put("/headlines/:id", function (req, res) {
-    db.Article.findOneAndUpdate({_id: req.params.id }, {
-        $set: {saved: req.body.saved}
-    })
-    .then(function (dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function (err) {
-        res.json(err);
-    });
-})
-
-router.post("/headlines/:id", function (req, res) {
-    db.Note.create(req.body)
-    .then(function (dbNote) {
-        return dbArticle.findOneAndUpdate({_id: req.params.id}, {$push: {notes: dbNote._id} }, {new: true});
-    })
-    .then(function (dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function (err) {
-        res.json(err);
-    });
-})
-
-router.delete("/headlines/:articleId/note/:noteId", function (req, res) {
-    db.Note.deleteOne({_id: req.params.noteId })
-    .then(function () {
-        return db.Article.update({ _id: req.params.articleId}, {$pull: {notes: req.params.noteId} });
-    })
-    .then(function (dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function (err) {
-        res.json(err);
-    });
-})
-
-router.delete("/scrape", function (req, res) {
-    db.Article.deleteMany({})
-    .then(function () {
-        return db.Note.deleteMany({});
-    })
-    .then(function (dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function (err) {
-        res.json(err);
-    });
-})
-module.exports = router;
+    },
+    delete: function (query, cb) {
+        Headline.remove(query, cb);
+    },
+    get: function (query, cb) {
+        Headline.find(query)
+        .sort({
+            _id: -1
+        })
+        .exec(function(err, doc) {
+            cb(doc);
+        })
+    },
+    update: function (query, cb) {
+        Headline.update({_id: query._id}, {
+            $set: query
+        }, {}, cb);
+    }
+}
