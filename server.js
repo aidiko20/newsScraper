727316e0bd0aeaadf818834227a50574445da501
Aida Yrysbekova <aidiko20@gmail.com>
2019-10-08 10:54:33 -0400

    changed main and indx handlebars

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
 
 
-var PORT = process.env.PORT || 3000
+
+var PORT = process.env.PORT || 8000
 var app = express();
 var router = express.Router();
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(_dirname + "/public"));
-
-var exphbs = require ("express-handlebars");
+app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.use(router);
 
-var db = process.env.MONGODB_URI || "mongodb://localhost:mongoHadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+var exphbs = require ("express-handlebars");
 
-mongoose.connect(db, function (error) {
-    if (error) {
-        console.log(error)
-    }
-    else {
-        console.log("mongoose connection is succssful");
-    }
-});
 
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
